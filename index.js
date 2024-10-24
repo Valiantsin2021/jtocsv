92c507fefdde582c00303a9a693c915cd0d71e64
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2024-10-25 00:31:19 +0200

    feat: add -h arg flag support

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 
 import { saveJSONSToCSV } from './parser.js'
 const args = process.argv.slice(2)
+
+function showHelp() {
+  console.log(`
+Usage: npx jtocsv [options]
+
+Options:
+-h, --help      Show help message
+path           Path to the JSONs folder
+output         Path to save the output CSV file
+
+Examples:
+npx jtocsv ./data ./data.csv
+`)
+}
+function handleArgs() {
+  if (args.includes('-h') || args.includes('--help')) {
+    showHelp()
+    process.exit(0)
+  }
+
+  console.log('No arguments provided. Try -h or --help for usage information.')
+}
+
+handleArgs()
 if (args.length < 2) {
   console.error(
     "\x1b[31mPlease provide path to the JSONs folder and path to save the output CSV file\nEx: './data' './data.csv'\x1b[0m"
